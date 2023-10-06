b46457af9897dc38038bff0844127131e9751989
lucas-g-marins <lhintzmarins@gmail.com>
2023-10-05 17:03:17 -0700

    add migration and seeds folders

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: "localhost", // May need to change this as MySQL server is hosted on a different machine
-      user: process.env.DB_LOCAL_USER,
-      password: process.env.DB_LOCAL_PASSWORD,
-      database: process.env.DB_LOCAL_DBNAME,
+      host: process.env.DB_HOST, // May need to change this as MySQL server is hosted on a different machine
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: "./db/migrations",
