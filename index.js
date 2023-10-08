8a46461a23506ba1edd106caf0cf4797d517121e
lucas-g-marins <lhintzmarins@gmail.com>
2023-10-07 18:16:35 -0700

    set up API to get warehouses

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // express
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const cors = require("cors");
-app.use(cors());
+
+const PORT = process.env.PORT || 5050;
+
+const warehouseRoutes = require("./routes/warehouses");
+
+app.use("/warehouses", warehouseRoutes);
+
+app.get("/", (req, res) => {
+  res.send("It's the API");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,22 @@
 require("dotenv").config(); // Load environment variables
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
 module.exports = {
-  development: {
-    client: "mysql",
-    connection: {
-      host: process.env.DB_HOST, // May need to change this as MySQL server is hosted on a different machine
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
-    seeds: {
-      directory: "./db/seeds",
-    },
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST, // May need to change this as MySQL server is hosted on a different machine
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: "utf8",
+  },
+  migrations: {
+    directory: "./db/migrations",
+  },
+  seeds: {
+    directory: "./db/seeds",
   },
 };
