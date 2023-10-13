ff16d329f533984ed380c2d51483a054ca43ad18
Sarbjot Marwaha <sarbjoto1@outlook.com>
2023-10-12 20:35:32 -0400

    add-NewWareHouse

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-// express
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-const cors = require("cors");
-app.use(cors());
+const bodyParser = require('body-parser');
+const knex = require('knex');
+const warehouseRoutes = require('./routes/NewWareHouse'); // Import to newwarehouse file 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database connection using knex
+const db = knex({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+// Use the NewWarehouse route
+app.use('/api', warehouseRoutes);
+
+// Start your Express server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ validation.js
@@ -0,0 +1,36 @@
+function validateWarehouseData(data) {
+    if (!data || typeof data !== 'object') {
+      return false; // Data is missing or not an object
+    }
+  
+    if (typeof data.warehouse_name !== 'string' || data.warehouse_name.trim() === '') {
+      return false; // Warehouse name is missing or not a non-empty string
+    }
+  
+    if (typeof data.address !== 'string' || data.address.trim() === '') {
+      return false; // Address is missing or not a non-empty string
+    }
+  
+    if (typeof data.city !== 'string' || data.city.trim() === '') {
+      return false; // City is missing or not a non-empty string
+    }
+  
+    if (typeof data.country !== 'string' || data.country.trim() === '') {
+      return false; // Country is missing or not a non-empty string
+    }
+  
+    if (typeof data.contact_name !== 'string' || data.contact_name.trim() === '') {
+      return false; // Contact name is missing or not a non-empty string
+    }
+  
+    if (typeof data.contact_position !== 'string' || data.contact_position.trim() === '') {
+      return false; // Contact position is missing or not a non-empty string
+    }
+  
+    // You can add more specific checks for phone number and email formats
+  
+    return true; // All validation checks pass
+  }
+  
+  module.exports = { validateWarehouseData };
+  
