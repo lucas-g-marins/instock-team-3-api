dcdf1b34b5516c689e528bfa3c39e707b03b4332
Sarbjot Marwaha <sarbjoto1@outlook.com>
2023-10-14 13:32:10 -0400

    add-app

--- app.js
+++ app.js
@@ -8,50 +8,29 @@ app.use(express.json());
 // Create a router for the inventory API
 const inventoryRouter = express.Router();
 
-// Define a route for retrieving a single inventory item
-inventoryRouter.get("/:id", async (req, res) => {
-  const itemId = req.params.id;
+// ... (your existing routes for retrieving and creating inventory items)
+
+// Define a new route for retrieving inventories for a given warehouse ID
+inventoryRouter.get("/:warehouseId/inventories", async (req, res) => {
+  const warehouseId = req.params.warehouseId;
 
   try {
     // Use Knex to query the "inventory" table in your database
-    const inventoryItem = await knex("inventory")
-      .select(
-        "id",
-        "warehouse_name",
-        "item_name",
-        "description",
-        "category",
-        "status",
-        "quantity"
-      )
-      .where("id", itemId) // Filter by the provided item ID
-      .first();
+    const inventories = await knex("inventory")
+      .select("id", "item_name", "category", "status", "quantity")
+      .where("warehouse_id", warehouseId);
 
-    if (!inventoryItem) {
-      // If no item is found with the given ID, return a 404 Not Found response
-      res.status(404).json({ error: "Item not found" });
+    if (!inventories || inventories.length === 0) {
+      // If no inventories are found for the given warehouse, return a 404 Not Found response
+      res
+        .status(404)
+        .json({ error: "No inventories found for this warehouse" });
     } else {
-      // If an item is found, return a 200 OK response with the item details
-      res.status(200).json(inventoryItem);
+      // If inventories are found, return a 200 OK response with the inventory list
+      res.status(200).json(inventories);
     }
   } catch (error) {
     // Handle any unexpected errors with a 500 Internal Server Error response
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// Define a route for creating a new inventory item
-inventoryRouter.post("/", async (req, res) => {
-  const newItem = req.body; // The new item details are sent in the request body
-
-  try {
-    // Use Knex to insert the new item into the "inventory" table
-    const [insertedId] = await knex("inventory").insert(newItem);
-    res.status(201).json({
-      id: insertedId,
-      ...newItem,
-    });
-  } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
