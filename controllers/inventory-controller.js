0c3c88ff27be18f9cd31df7e29ff7bc7105b69e4
lucas-g-marins <lhintzmarins@gmail.com>
2023-10-14 17:12:05 -0700

    get params for put request

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -8,10 +8,10 @@ const index = (_req, res) => {
     .catch((err) => res.status(400).send(`Error retrieving inventory: ${err}`));
 };
 
-const inventoryItem = (_req, res) => {
+const inventoryItem = (req, res) => {
   knex
     .from("inventories")
-    .where({ id: 1 })
+    .where({ id: req.params.id })
     .then((data) => {
       res.status(200).json(data);
     })
@@ -21,6 +21,7 @@ const inventoryItem = (_req, res) => {
 };
 
 const editInventory = (req, res) => {
+  console.log("body", req.body);
   knex
     .from("inventories")
     .where({ id: req.params.id })
